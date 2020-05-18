dfe3e1b54fd0b519d6cbe00b388232989d496800
ciposneros <ncisneros.ing@gmail.com>
2020-05-18 16:14:36 -0500

    test

---
+++ app/src/main/java/com/example/misuperparte2/Intenciones.java
@@ -0,0 +1,14 @@
+package com.example.misuperparte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intenciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intenciones);
+    }
+}
